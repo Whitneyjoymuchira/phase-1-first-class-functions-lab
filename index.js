a785d7752a772f1cd61d4ac3ad9d2c33db56df1c
Whitney Muchira <whitneymuchira@gmail.com>
2022-06-09 08:29:42 +0300

    commit tree a0ae76d7205b11bdb2cfa281cbda9cb991550810

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+ const FirstTwoDrivers= drivers
+ function returnFirstTwoDrivers(){
+     
+     return  drivers.slice(0,2)
+  
+ }
+
+ const returnLastTwoDrivers= function (){
+     return drivers.slice(-2)
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+   
+   const createFareMultiplier= function (fareMultiplier) {
+     return function (fare) {
+         return fareMultiplier * fare
+     }
+ }
+ const fareDoubler = createFareMultiplier(2)
+ const fareTripler = createFareMultiplier(3)
+ 
+ const selectDifferentDrivers = function name(drivers, arrayOfDrivers) {
+     return arrayOfDrivers(drivers)
+ }
+ 
+
+ 
